96b266d80543c240d68fa1ce596df684a9b2f34c
Varun <varunrajanna9@gmail.com>
2021-06-01 17:48:16 +0530

    Initial commit

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 
 output "droplet_ip_address" {
-  value = "Drop Ip address is ${digitalocean_droplet.webServer.ipv4_address}"
+  value = "Droplet Ip address is ${digitalocean_droplet.webServer.ipv4_address}"
 }
